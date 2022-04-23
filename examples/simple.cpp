a79e24f98534e354d14c62c176399413c6c2fb1f
MerelyLogical <qieerbushe7@gmail.com>
2022-04-23 18:06:30 +0100

    and sine wave and rest

--- examples/simple.cpp
+++ examples/simple.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <unordered_map>
 #include <cmath>
+#include <cassert>
 
 #define S_RATE 44100
 #define BPM 100
@@ -23,30 +24,59 @@ typedef struct WAV_HEADER {
     uint32_t data_size     = 0;                  // calculate and fill in later
 } wav_hdr_t;
 
-// make a square wave
-void square(std::ofstream& fout, uint32_t& data_size,
-            unsigned int length, unsigned int freq) {
+enum shape_t {none, sine, square, triangle, saw};
+
+// write one note
+void play(std::ofstream& fout, uint32_t& data_size, shape_t shape,
+          unsigned int length, float freq = 0.0, int octave = 0) {
+
+    assert(length != 0);
+    if(shape != none) { assert(freq != 0.0 && octave != 0); }
+
+    freq = freq * pow(2.0, octave-4);
     uint16_t pcm_data;
-    bool sign = true;
-    float half_period = S_RATE/(freq/2.0);
-    float count = half_period;
     float smqvr = 15.0/BPM;
     float s_len = smqvr*length*S_RATE; // length in number of samples
 
+    // only used for square
+    bool sign = true;
+    float half_period = S_RATE/freq/2.0;
+    float count = half_period;
+
     for(unsigned int i = 0; i < round(s_len); ++i) {
-        if (sign) {
-            pcm_data = 2000;
-        } else {
-            pcm_data = -2000;
+        switch (shape) {
+            case none:
+                pcm_data = 0;
+                break;
+
+            case sine:
+                pcm_data = round(6000.0 * sin(2.0*M_PI*freq*i/S_RATE));
+                break;
+
+            case square:
+                if (sign) {
+                    pcm_data = 2000;
+                } else {
+                    pcm_data = -2000;
+                }
+
+                count -= 1.0;
+                if (count < 0) {
+                    count += half_period;
+                    sign = !sign;
+                }
+                break;
+
+            case triangle:
+                // TODO
+                break;
+
+            case saw:
+                // TODO
+                break;
         }
         fout.write(reinterpret_cast<char *>(&pcm_data), sizeof(uint16_t));
         data_size += sizeof(uint16_t);
-
-        count -= 1.0;
-        if (count < 0) {
-            count += half_period;
-            sign = !sign;
-        }
     }
 }
 
@@ -60,7 +90,7 @@ int main(void) {
         {"Ab", -1}, {"A", 0}, {"As", 1}, {"Bb", 1}, {"B", 2}
     };
 
-    for(auto& i : freq_lut) {
+    for(auto& i: freq_lut) {
         i.second = 440.0 * pow(2.0, i.second/12);
     }
 
@@ -72,12 +102,72 @@ int main(void) {
     fout.open("m.wav", std::ios::binary);
     fout.write(reinterpret_cast<const char *>(&wav_hdr), sizeof(wav_hdr_t));
 
-    square(fout, data_size, 2, freq_lut["C"]);
-    square(fout, data_size, 2, freq_lut["Eb"]);
-    square(fout, data_size, 2, freq_lut["F"]);
-    square(fout, data_size, 2, freq_lut["G"]);
-    square(fout, data_size, 2, freq_lut["A"]);
-    square(fout, data_size, 4, freq_lut["C"]);
+    // TODO: make this simpler to write
+    play(fout, data_size, none,   1);
+    play(fout, data_size, sine,   2, freq_lut["Eb"], 4);
+    play(fout, data_size, sine,   2, freq_lut["F"],  4);
+    play(fout, data_size, sine,   2, freq_lut["G"],  4);
+    play(fout, data_size, sine,   2, freq_lut["Bb"], 4);
+    play(fout, data_size, sine,   2, freq_lut["G"],  4);
+    play(fout, data_size, sine,   3, freq_lut["G"],  4);
+    play(fout, data_size, none,   1);
+    play(fout, data_size, sine,   1, freq_lut["F"],  4);
+    play(fout, data_size, sine,   1, freq_lut["F"],  4);
+    play(fout, data_size, sine,   2, freq_lut["Eb"], 4);
+    play(fout, data_size, sine,   3, freq_lut["F"],  4);
+    play(fout, data_size, none,   1);
+    play(fout, data_size, sine,   2, freq_lut["Eb"], 4);
+    play(fout, data_size, sine,   2, freq_lut["C"],  4);
+    play(fout, data_size, sine,   2, freq_lut["Eb"], 4);
+    play(fout, data_size, sine,   2, freq_lut["F"],  4);
+    play(fout, data_size, sine,   5, freq_lut["G"],  4);
+    play(fout, data_size, none,   3);
+    play(fout, data_size, sine,   2, freq_lut["Eb"], 4);
+    play(fout, data_size, sine,   2, freq_lut["C"],  4);
+    play(fout, data_size, sine,   3, freq_lut["Eb"], 4);
+    play(fout, data_size, none,   1);
+    play(fout, data_size, sine,   1, freq_lut["Bb"], 3);
+    play(fout, data_size, sine,   1, freq_lut["Bb"], 3);
+    play(fout, data_size, sine,   2, freq_lut["F"],  4);
+    play(fout, data_size, sine,   3, freq_lut["Eb"], 4);
+    play(fout, data_size, none,   1);
+    play(fout, data_size, sine,   2, freq_lut["G"],  4);
+    play(fout, data_size, sine,   2, freq_lut["F"],  4);
+    play(fout, data_size, sine,   2, freq_lut["F"],  4);
+    play(fout, data_size, sine,   2, freq_lut["Eb"], 4);
+    play(fout, data_size, sine,   4, freq_lut["F"],  4);
+    play(fout, data_size, square, 2, freq_lut["Eb"], 4);
+    play(fout, data_size, square, 2, freq_lut["F"],  4);
+    play(fout, data_size, square, 2, freq_lut["Bb"], 4);
+    play(fout, data_size, square, 2, freq_lut["G"],  4);
+    play(fout, data_size, square, 3, freq_lut["G"],  4);
+    play(fout, data_size, none,   1);
+    play(fout, data_size, square, 1, freq_lut["F"],  4);
+    play(fout, data_size, square, 1, freq_lut["F"],  4);
+    play(fout, data_size, square, 2, freq_lut["Eb"], 4);
+    play(fout, data_size, square, 3, freq_lut["F"],  4);
+    play(fout, data_size, none,   1);
+    play(fout, data_size, square, 2, freq_lut["Eb"], 4);
+    play(fout, data_size, square, 2, freq_lut["C"],  4);
+    play(fout, data_size, square, 2, freq_lut["Eb"], 4);
+    play(fout, data_size, square, 2, freq_lut["Bb"], 4);
+    play(fout, data_size, square, 5, freq_lut["G"],  4);
+    play(fout, data_size, none,   3);
+    play(fout, data_size, square, 2, freq_lut["Eb"], 4);
+    play(fout, data_size, square, 2, freq_lut["C"],  4);
+    play(fout, data_size, square, 3, freq_lut["Eb"], 4);
+    play(fout, data_size, none,   1);
+    play(fout, data_size, square, 1, freq_lut["Bb"], 3);
+    play(fout, data_size, square, 1, freq_lut["Bb"], 3);
+    play(fout, data_size, square, 2, freq_lut["F"],  4);
+    play(fout, data_size, square, 3, freq_lut["Eb"], 4);
+    play(fout, data_size, none,   1);
+    play(fout, data_size, square, 2, freq_lut["G"],  4);
+    play(fout, data_size, square, 2, freq_lut["F"],  4);
+    play(fout, data_size, square, 2, freq_lut["Eb"], 4);
+    play(fout, data_size, square, 2, freq_lut["C"],  4);
+    play(fout, data_size, square, 4, freq_lut["Eb"], 4);
+
 
     // calculate header and overwrite with correct size bits
     wav_hdr.data_size = data_size;
