dbe596cf1642736aaccabd07e1374d5c1ec1e9c7
MerelyLogical <qieerbushe7@gmail.com>
2022-05-01 00:58:54 +0100

    add a bad implementation of polyphony

--- examples/simple.cpp
+++ examples/simple.cpp
@@ -4,6 +4,7 @@
 #include <unordered_map>
 #include <cmath>
 #include <cassert>
+#include <vector>
 
 #define S_RATE 44100
 #define BPM 100
@@ -15,9 +16,9 @@
 typedef struct Wav_Header wav_hdr_t;
 enum shape_t: uint8_t;
 class note_t;
-void play(std::ofstream &fout, uint32_t &data_size, note_t note);
-void play(std::ofstream &fout, uint32_t &data_size, shape_t shape,
-          unsigned int length, float freq, int octave);
+void play(std::fstream &f, uint32_t &data_size, note_t note, bool first);
+void play(std::fstream &f, uint32_t &data_size, shape_t shape,
+          unsigned int length, float freq, int octave, bool first);
 float filter(int i, unsigned int s_len);
 
 typedef struct Wav_Header {
@@ -44,9 +45,9 @@ private:
     typedef std::unordered_map<std::string, float> f_lut_t;
     static f_lut_t construct_lut() {
         f_lut_t f_lut = {
-            {"C", -9}, {"Cs", -8}, {"Db", -8}, {"D", -7}, {"Ds", -6}, {"Eb", -6},
-            {"E", -5}, {"F", -4}, {"Fs", -3}, {"Gb", -3}, {"G", -2}, {"Gs", -1},
-            {"Ab", -1}, {"A", 0}, {"As", 1}, {"Bb", 1}, {"B", 2}
+            {"C", -9}, {"C+", -8}, {"D-", -8}, {"D", -7}, {"D+", -6}, {"E-", -6},
+            {"E", -5}, {"F", -4}, {"F+", -3}, {"G-", -3}, {"G", -2}, {"G+", -1},
+            {"A-", -1}, {"A", 0}, {"A+", 1}, {"B-", 1}, {"B", 2}
         };
 
         for (auto &i: f_lut) { i.second = 440.0 * pow(2.0, i.second/12); }
@@ -91,13 +92,13 @@ float filter(int i, unsigned int s_len) {
 }
 
 // write one note with note_t
-void play(std::ofstream &fout, uint32_t &data_size, note_t note) {
-    play(fout, data_size, note.shape, note.length, note.freq, note.octave);
+void play(std::fstream &f, uint32_t &data_size, note_t note, bool first) {
+    play(f, data_size, note.shape, note.length, note.freq, note.octave, first);
 }
 
 // write one note with note parameters
-void play(std::ofstream &fout, uint32_t &data_size, shape_t shape,
-          unsigned int length, float freq = 0.0, int octave = 4) {
+void play(std::fstream &f, uint32_t &data_size, shape_t shape,
+          unsigned int length, float freq = 0.0, int octave = 4, bool first = false) {
 
     assert(length != 0);
     if (shape != none) { assert(freq != 0.0 && octave != 0); }
@@ -105,7 +106,8 @@ void play(std::ofstream &fout, uint32_t &data_size, shape_t shape,
     freq = freq * pow(2.0, octave-4);
     float wave;
     float gain;
-    int16_t pcm_data;
+    int16_t pcm_out;
+    int16_t pcm_in;
     static float smqvr = 15.0/BPM;
     unsigned int s_len = rint(smqvr*length*S_RATE); // length in number of samples
 
@@ -136,7 +138,7 @@ void play(std::ofstream &fout, uint32_t &data_size, shape_t shape,
     for (unsigned int i = 0; i < s_len; ++i) {
         switch (shape) {
             case none:
-                pcm_data = 0;
+                wave = 0;
                 break;
 
             case sine:
@@ -174,9 +176,17 @@ void play(std::ofstream &fout, uint32_t &data_size, shape_t shape,
         } else {
             gain = filter((int)i, s_len);
         }
-        pcm_data = gain * wave;
-        fout.write(reinterpret_cast<char *>(&pcm_data), sizeof(uint16_t));
-        data_size += sizeof(uint16_t);
+
+        pcm_out = gain * wave;
+        if (first) {
+            f.write(reinterpret_cast<char *>(&pcm_out), sizeof(uint16_t));
+            data_size += sizeof(uint16_t);
+        } else {
+            f.read(reinterpret_cast<char *>(&pcm_in), sizeof(uint16_t));
+            f.seekp(-sizeof(uint16_t), std::ios::cur);
+            pcm_out += pcm_in;
+            f.write(reinterpret_cast<char *>(&pcm_out), sizeof(uint16_t));
+        }
     }
 }
 
@@ -187,87 +197,89 @@ int main(void) {
     wav_hdr_t wav_hdr;
     uint32_t data_size = 0;
 
-    std::ofstream fout;
-    fout.open("m.wav", std::ios::binary);
-    fout.write(reinterpret_cast<const char *>(&wav_hdr), sizeof(wav_hdr_t));
+    std::fstream f;
+    f.open("m.wav", std::ios::in | std::ios::out | std::ios::binary);
+    f.write(reinterpret_cast<const char *>(&wav_hdr), sizeof(wav_hdr_t));
 
-    note_t score[] = {
-        {none,   1},
-        {sine,   2, "Eb", 4},
-        {sine,   2, "F",  4},
-        {sine,   2, "G",  4},
-        {sine,   2, "Bb", 4},
-        {sine,   2, "G",  4},
-        {sine,   3, "G",  4},
-        {none,   1},
-        {sine,   1, "F",  4},
-        {sine,   1, "F",  4},
-        {sine,   2, "Eb", 4},
-        {sine,   3, "F",  4},
-        {none,   1},
-        {sine,   2, "Eb", 4},
-        {sine,   2, "C",  4},
-        {sine,   2, "Eb", 4},
-        {sine,   2, "F",  4},
-        {sine,   5, "G",  4},
-        {none,   3},
-        {sine,   2, "Eb", 4},
-        {sine,   2, "C",  4},
-        {sine,   3, "Eb", 4},
-        {none,   1},
-        {saw,    1, "Bb", 3},
-        {saw,    1, "Bb", 3},
-        {saw,    2, "F",  4},
-        {saw,    3, "Eb", 4},
-        {none,   1},
-        {saw,    2, "G",  4},
-        {saw,    2, "F",  4},
-        {saw,    2, "F",  4},
-        {saw,    2, "Eb", 4},
-        {saw,    4, "F",  4},
-        {saw,    2, "Eb", 4},
-        {saw,    2, "F",  4},
-        {saw,    2, "Bb", 4},
-        {saw,    2, "G",  4},
-        {saw,    3, "G",  4},
-        {none,   1},
-        {square, 1, "F",  4},
-        {square, 1, "F",  4},
-        {square, 2, "Eb", 4},
-        {square, 3, "F",  4},
-        {none,   1},
-        {square, 2, "Eb", 4},
-        {square, 2, "C",  4},
-        {square, 2, "Eb", 4},
-        {square, 2, "Bb", 4},
-        {square, 5, "G",  4},
-        {none,   3},
-        {square, 2, "Eb", 4},
-        {square, 2, "C",  4},
-        {square, 3, "Eb", 4},
-        {none,   1},
-        {square, 1, "Bb", 3},
-        {square, 1, "Bb", 3},
-        {square, 2, "F",  4},
-        {square, 3, "Eb", 4},
-        {none,   1},
-        {square, 2, "G",  4},
-        {square, 2, "F",  4},
-        {square, 2, "Eb", 4},
-        {square, 2, "C",  4},
-        {square, 4, "Eb", 4},
+    std::vector<std::vector<note_t>> score = {
+        // stave 1
+        { {square, 4,  "B",  3},
+          {square, 2,  "C+", 4},
+          {square, 2,  "E",  4},
+          {square, 4,  "F+", 4},
+          {square, 4,  "G+", 4},
+
+          {square, 16, "G+", 4},
+
+          {square, 4,  "B",  3},
+          {square, 2,  "C+", 4},
+          {square, 2,  "E",  4},
+          {square, 4,  "F+", 4},
+          {square, 4,  "G+", 4},
+
+          {square, 6,  "G+", 4},
+          {square, 2,  "F+", 4},
+          {square, 4,  "F+", 4},
+          {square, 2,  "B",  3},
+          {square, 2,  "B",  3},
+        },
+
+        // stave 2
+        { {saw, 4,  "B",  2},
+          {saw, 4,  "E",  3},
+          {saw, 4,  "F+", 3},
+          {saw, 4,  "G+", 3},
+
+          {saw, 4,  "B",  3},
+          {saw, 4,  "C+", 4},
+          {saw, 4,  "E",  4},
+          {saw, 4,  "F+", 4},
+
+          {saw, 4,  "E",  4},
+          {saw, 4,  "C+", 4}, // overlaps with C+ in stave 1, sounds a bit weird
+          {saw, 4,  "B",  3},
+          {saw, 4,  "G+", 3},
+
+          {saw, 4,  "E",  3},
+          {saw, 4,  "C+", 3},
+          {saw, 4,  "B",  2},
+          {saw, 4,  "A",  2},
+        },
+
+        // stave 3
+        { {sine, 8, "E",  2},
+          {sine, 8, "B",  2},
+
+          {sine, 8, "C+", 2},
+          {sine, 8, "G+", 2},
+
+          {sine, 8, "A",  2},
+          {sine, 8, "G+", 2},
+
+          {sine, 8, "F+", 2},
+          {sine, 8, "B",  2},
+        }
     };
 
-    for (const note_t &note: score) {
-        play(fout, data_size, note);
+    // data_size depends on length of first stave
+    // NOTE: buggy output if first stave isn't the longest. fix?
+    // TODO: requires repeated reading and writing over the same file, slow
+    //       not sure how to fix except for writing everything into memory first
+    bool first = true;
+    for (const auto &stave: score) {
+        for (const auto &note: stave) {
+            play(f, data_size, note, first);
+        }
+        first = false;
+        f.seekp(sizeof(wav_hdr_t));
     }
 
     // calculate header and overwrite with correct size bits
     wav_hdr.data_size = data_size;
     wav_hdr.file_size = data_size + 32;
-    fout.seekp(0);
-    fout.write(reinterpret_cast<const char *>(&wav_hdr), sizeof(wav_hdr_t));
-    fout.close();
+    f.seekp(0);
+    f.write(reinterpret_cast<const char *>(&wav_hdr), sizeof(wav_hdr_t));
+    f.close();
 
     return 0;
 }
