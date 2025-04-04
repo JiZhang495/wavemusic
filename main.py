37b332497260db702ca5de012926463185721d43
Ji Zhang <zhangji1623316718@gmail.com>
2025-04-04 18:18:22 +0100

    refactor: minor edits in main

--- main.py
+++ main.py
@@ -21,36 +21,35 @@ def main_gui():
         print(f"Error running tkinter: {e}")
         root.destroy()
         print("Starting in CLI mode...")
-        main_cli()
+        main()
 
-def main_cli(*args):
+def main(*args):
     # Command line interface for playing music
     print("Hello from wavemusic!")
-    if len(args) > 0:
-        if args[0] == "gui":
-            main_gui()
+    if args[0] == "gui":
+        main_gui()
+        return
+    if args[0] == "cli":
+        pass
+    else:
+        try:
+            score_file = args[0].replace(" ", "").replace("\n", "").replace("\r", "")
+            with open(score_file, "r") as f:
+                score = f.read()
+            print(f"Score loaded from {score_file}")
+        except FileNotFoundError:
+            print(f"File {score_file} not found. Using default score.")
+            score = "s1r 2eb 2f 2g | 2bb 2g 3g 1r | 1f 1f 2eb 3f 1r | 2eb 2c 2eb 2f | 5g 3r | t2eb " \
+                    "2c 3eb 1r | 1bb3 1bb3 2f4 3eb 1r | 2g 2f 2f 2eb | 4f q2eb 2f | 2bb 2g 3g 1r | 1f 1f 2eb 3f 1r | "\
+                    "2eb 2c 2eb 2bb | 5g 3r | w2eb 2c 3eb 1r | 1bb3 1bb3 2f4 3eb 1r | 2g 2f 2eb 2c | 4eb"
+        print(score)
+        try:
+            Music(score).playscore(filename="m.wav", sample_rate=44100, bpm=100)
+            print("Playing score...")
             return
-        if args[0] == "cli":
-            pass
-        else:
-            try:
-                score_file = args[0].replace(" ", "").replace("\n", "").replace("\r", "")
-                with open(score_file, "r") as f:
-                    score = f.read()
-                print(f"Score loaded from {score_file}")
-            except FileNotFoundError:
-                print(f"File {score_file} not found. Using default score.")
-                score = "s1r 2eb 2f 2g | 2bb 2g 3g 1r | 1f 1f 2eb 3f 1r | 2eb 2c 2eb 2f | 5g 3r | t2eb " \
-                        "2c 3eb 1r | 1bb3 1bb3 2f4 3eb 1r | 2g 2f 2f 2eb | 4f q2eb 2f | 2bb 2g 3g 1r | 1f 1f 2eb 3f 1r | "\
-                        "2eb 2c 2eb 2bb | 5g 3r | w2eb 2c 3eb 1r | 1bb3 1bb3 2f4 3eb 1r | 2g 2f 2eb 2c | 4eb"
-            print(score)
-            try:
-                Music(score).playscore(filename="m.wav", sample_rate=44100, bpm=100)
-                print("Playing score...")
-                return
-            except Exception as e:
-                print(f"Error playing score: {e}")
-                return
+        except Exception as e:
+            print(f"Error playing score: {e}")
+            return
 
     score = input("Enter your score (enter :q to finish entry): ")
     if score == ":q":
@@ -72,7 +71,7 @@ def main_cli(*args):
     
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1 and sys.argv[1] != "gui":
-        main_cli(*sys.argv[1:])
+    if len(sys.argv) > 1:
+        main(*sys.argv[1:])
     else:
         main_gui()
