d6b4684fb3cd0a4dac34bf81d95610275ffef5d8
Ji Zhang <zhangji1623316718@gmail.com>
2025-04-04 17:47:17 +0100

    add cli in case gui unavailable

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python3
 from scripts.music import Music
 import tkinter as tk
+import sys
 from scripts.gui import WaveMusicGUI
-
-def main():
-    print("Hello from wavemusic!")
-    score = "s1r 2eb 2f 2g | 2bb 2g 3g 1r | 1f 1f 2eb 3f 1r | 2eb 2c 2eb 2f | 5g 3r | t2eb " \
-    "2c 3eb 1r | 1bb3 1bb3 2f4 3eb 1r | 2g 2f 2f 2eb | 4f q2eb 2f | 2bb 2g 3g 1r | 1f 1f 2eb 3f 1r | "\
-    "2eb 2c 2eb 2bb | 5g 3r | w2eb 2c 3eb 1r | 1bb3 1bb3 2f4 3eb 1r | 2g 2f 2eb 2c | 4eb"
-    Music(score).playscore(filename="m.wav", sample_rate=44100, bpm=100)
+from scripts.music import Music
 
 class MainApp:
     def __init__(self, root):
@@ -18,10 +13,66 @@ class MainApp:
         self.gui.frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
 def main_gui():
-    root = tk.Tk()
-    app = MainApp(root)
-    root.mainloop()
+    try:
+        root = tk.Tk()
+        app = MainApp(root)
+        root.mainloop()
+    except Exception as e:
+        print(f"Error running tkinter: {e}")
+        root.destroy()
+        print("Starting in CLI mode...")
+        main_cli()
+
+def main_cli(*args):
+    # Command line interface for playing music
+    print("Hello from wavemusic!")
+    if len(args) > 0:
+        if args[0] == "gui":
+            main_gui()
+            return
+        if args[0] == "cli":
+            pass
+        else:
+            try:
+                score_file = args[0].replace(" ", "").replace("\n", "").replace("\r", "")
+                with open(score_file, "r") as f:
+                    score = f.read()
+                print(f"Score loaded from {score_file}")
+            except FileNotFoundError:
+                print(f"File {score_file} not found. Using default score.")
+                score = "s1r 2eb 2f 2g | 2bb 2g 3g 1r | 1f 1f 2eb 3f 1r | 2eb 2c 2eb 2f | 5g 3r | t2eb " \
+                        "2c 3eb 1r | 1bb3 1bb3 2f4 3eb 1r | 2g 2f 2f 2eb | 4f q2eb 2f | 2bb 2g 3g 1r | 1f 1f 2eb 3f 1r | "\
+                        "2eb 2c 2eb 2bb | 5g 3r | w2eb 2c 3eb 1r | 1bb3 1bb3 2f4 3eb 1r | 2g 2f 2eb 2c | 4eb"
+            print(score)
+            try:
+                Music(score).playscore(filename="m.wav", sample_rate=44100, bpm=100)
+                print("Playing score...")
+                return
+            except Exception as e:
+                print(f"Error playing score: {e}")
+                return
+
+    score = input("Enter your score (enter :q to finish entry): ")
+    if score == ":q":
+        print("No score entered. Exiting...")
+        return
+    input_score = ""
+    while input_score != ":q":
+        score += input_score + " "
+        input_score = input()
+    score = score.replace("|", "")  # remove the bar lines
+    if score is None or score == "":
+        print("No score entered. Exiting...")
+        return
+    try:
+        Music(score).playscore(filename="m.wav", sample_rate=44100, bpm=100)
+        print("Playing score...")
+    except Exception as e:
+        print(f"Error playing score: {e}")
+    
 
 if __name__ == "__main__":
-    # main()
-    main_gui()
+    if len(sys.argv) > 1 and sys.argv[1] != "gui":
+        main_cli(*sys.argv[1:])
+    else:
+        main_gui()
