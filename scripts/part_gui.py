a6294b2a870dc262328c2103aa3ca2cc35071d8c
Ji Zhang <zhangji1623316718@gmail.com>
2025-04-04 18:48:50 +0100

    fix: play_line bug of PartGUI object has no attribute 'master'

--- scripts/part_gui.py
+++ scripts/part_gui.py
@@ -5,6 +5,7 @@ from scripts.utils import play_wav
 
 class PartGUI:
     def __init__(self, master):
+        self.master = master
         self.part_frame = tk.Frame(master.frame)
         self.part_frame.pack(pady=5)
 
